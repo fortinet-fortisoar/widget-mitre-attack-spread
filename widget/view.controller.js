e37203f07af6c18574ad7dbe9ed8b809fe881f89
cs-pranali-chavan <pchavan@fortinet.com>
2024-01-23 19:49:45 +0530

    copyright added in js files

--- widget/view.controller.js
+++ widget/view.controller.js
@@ -1,3 +1,8 @@
+/* Copyright start
+  Copyright (C) 2008 - 2022 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end */
 'use strict';
 (function () {
   angular
